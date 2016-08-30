1e0ce5b2d72ec8c83e6bedcc3dad1dca2e0cabcf
wuxinshui <rfg2012@163.com>
2016-08-30 18:27:51 +0800

    LargestDifference

---
+++ codewars/src/main/java/org/noob/codewars/train/learn/prac160830/LargestDifference.java
@@ -0,0 +1,25 @@
+package org.noob.codewars.train.learn.prac160830;
+
+import java.util.Arrays;
+
+/**
+ * Created by FujiRen on 2016/8/30.
+ */
+public class LargestDifference {
+
+    public static void main(String[] args) {
+        int[] s=new int[]{9, 4, 1, 10, 3, 4, 0, -1, -2};
+//        Arrays.sort(s);
+        String string=Arrays.toString(s);
+        System.out.println(Arrays.toString(s));
+        System.out.println(string.indexOf("10")-string.indexOf("-2"));
+    }
+
+
+    public static int largestDifference(int[] data) {
+
+        //code here
+        return 0;
+
+    }
+}
