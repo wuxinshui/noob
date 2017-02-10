d2cd08bb047949a41407e054e3f0f420b2639324
wuxinshui <rfg2012@163.com>
2017-02-10 10:00:55 +0800

    序列化验证。枚举实现最简洁的单例模式，无偿地提供了序列化机制，绝对防止多次实例化

--- effective-java/src/main/java/chapter2/item3/Client.java
+++ effective-java/src/main/java/chapter2/item3/Client.java
@@ -1,12 +1,27 @@
 package chapter2.item3;
 
+import java.io.*;
+
 /**
  * Created by Administrator on 2017/2/8.
  */
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println(Elvis.INSTANCE);
         Elvis elvis=Elvis.INSTANCE;
         elvis.leaveTheBuilding();
+
+        //序列化和反序列化
+        FileOutputStream fos=new FileOutputStream("elvis.txt");
+		ObjectOutputStream oos=new ObjectOutputStream(fos);
+		oos.writeObject(elvis);
+		oos.flush();
+
+		FileInputStream fis=new FileInputStream("elvis.txt");
+		ObjectInputStream ois=new ObjectInputStream(fis);
+		 Elvis elvis1= (Elvis) ois.readObject();
+
+		System.out.println("序列化前的对象和序列化后的对象比较："+(elvis==elvis1));
+
     }
 }
