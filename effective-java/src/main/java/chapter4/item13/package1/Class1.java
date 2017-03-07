c622ac5b441fccd7e40ca7e16441cf88c7696182
rfg2012@163.com <security2012>
2017-03-08 07:44:32 +0800

    //默认访问级别：包级私有的。只能在同一个包内访问，不能跨包访问

---
+++ effective-java/src/main/java/chapter4/item13/package1/Class1.java
@@ -0,0 +1,9 @@
+package chapter4.item13.package1;
+
+/**
+ * Created by Administrator on 2017/3/8.
+ */
+//默认访问级别：包级私有的。只能在同一个包内访问，不能跨包访问
+ class Class1 {
+
+}
---
+++ effective-java/src/main/java/chapter4/item13/package2/Class2.java
@@ -0,0 +1,12 @@
+package chapter4.item13.package2;
+
+
+/**
+ * Created by Administrator on 2017/3/8.
+ */
+public class Class2 {
+    public static void main(String[] args) {
+        //默认访问级别：包级私有的。只能在同一个包内访问，不能跨包访问
+        //Class1 class1=new Class1();
+    }
+}
