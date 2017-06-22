7036006d3af5a84b32887843b6b0dea37f17615e
rfg2012@163.com <security2012>
2017-06-22 23:15:11 +0800

    消除非受检警告

---
+++ effective-java/src/main/java/chapter4/item24/SuppressWarningsTest.java
@@ -0,0 +1,13 @@
+package chapter4.item24;
+
+/**
+ * <p>SuppressWarningsTest</p>
+ * 消除非受检警告
+ *
+ * @author wuxinshui
+ */
+public class SuppressWarningsTest {
+//    1.@SuppressWarnings("unchecked")注解可以用在任何粒度的级别中，应在尽可能小的范围中使用（包括局部变量的声明中）。
+//    2.将注解放在return语句中时非法的，因为它不是一个声明[JLS,9.7]。
+//    3.用注释把禁止警告的原因记录下来。
+}
