abbae4dd8a38851d1a06ccc14c2915cbc2811f07
rfg2012@163.com <security2012>
2017-06-24 13:21:59 +0800

    注释自动缩进测试

---
+++ effective-java/src/main/java/chapter4/item25/CodeCommentTest.java
@@ -0,0 +1,14 @@
+package chapter4.item25;
+
+/**
+ * <p>CodeCommentTest</p>
+ *
+ * @author wuxinshui
+ */
+public class CodeCommentTest {
+    public static void main(String[] args) {
+//        这是一个没有自动缩进的注释
+        //这是一个会自动缩进的注释
+        System.out.println("注释自动缩进测试");
+    }
+}
