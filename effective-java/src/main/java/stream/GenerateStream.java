b2cd35b07af852bf39d0f48d6a437871c39a33ee
任富吉 <fuji.ren@bkjk.com>
2019-06-27 15:20:54 +0800

    generate stream

--- effective-java/src/main/java/stream/GenerateStream.java
+++ effective-java/src/main/java/stream/GenerateStream.java
@@ -1,5 +1,8 @@
 package stream;
 
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
@@ -9,7 +12,29 @@ import java.util.stream.Stream;
  */
 public class GenerateStream {
     public static void main(String[] args) {
+        System.out.println("Stream.of Integer--------------------");
         Stream.of(2).forEach(System.out::println);
-        Stream.of(2,3,4,"aa").forEach(System.out::println);
+        System.out.println("Stream.of Integer String--------------------");
+        Stream.of(2, 3, 4, "aa").forEach(System.out::println);
+        System.out.println("Stream.of array--------------------");
+        String[] array = new String[]{"12", "24"};
+        Stream.of(array).forEach(System.out::println);
+        System.out.println("Stream.concat--------------------");
+        Stream.concat(Stream.of(12), Stream.of(34)).forEach(System.out::println);
+        System.out.println("List.of String--------------------");
+        List.of("aa", "er").stream().sorted().forEach(System.out::println);
+        System.out.println("List.of Integer String--------------------");
+        //java.lang.ClassCastException: java.base/java.lang.String cannot be cast to java.base/java.lang.Integer
+        //List.of("aa",23,"23","34").stream().sorted().forEach(System.out::println);
+        //Exception in thread "main" java.lang.ClassCastException: java.base/java.lang.Integer cannot be cast to java.base/java.lang.String
+        //List.of(23,"23","34").stream().sorted().forEach(System.out::println);
+        List.of("aa", 23, "23", "34").stream().forEach(System.out::println);
+        System.out.println("List.of String Integer--------------------");
+        List.of(23, "23", "34").stream().forEach(System.out::println);
+        System.out.println("Stream.generate--------------------");
+        Random random = new Random();
+        Supplier<Integer> supplier = random::nextInt;
+        Stream.generate(supplier).limit(5).forEach(System.out::println);
+
     }
 }
