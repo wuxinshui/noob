c060ea1b21b684f06a495469ce3a56fd5eed1163
任富吉 <fuji.ren@bkjk.com>
2019-07-03 14:37:04 +0800

    test predicate

--- effective-java/src/main/java/java8/functioninterface/PredicateTest.java
+++ effective-java/src/main/java/java8/functioninterface/PredicateTest.java
@@ -13,6 +13,7 @@ public class PredicateTest {
     public static void main(String[] args) {
         List<Integer> list = List.of(1, 2, 3, 4, 5);
         eval(list, n -> n > 3);
+        list.stream().filter(n->n>3).forEach(System.out::println);
     }
 
 
