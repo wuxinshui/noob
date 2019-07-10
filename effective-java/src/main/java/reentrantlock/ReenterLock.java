8ec17848b6674d8f111102c74031cd39aaca7c62
白芷 <2397701392@qq.com>
2019-07-11 00:16:58 +0800

    add reentrantLock 重入锁

--- effective-java/src/main/java/reentrantlock/ReenterLock.java
+++ effective-java/src/main/java/reentrantlock/ReenterLock.java
@@ -9,18 +9,22 @@ public class ReenterLock implements Runnable {
     private static ReentrantLock lock = new ReentrantLock();
     private static int i = 0;
 
-    public ReenterLock(ReentrantLock lock) {
-        this.lock = lock;
-    }
+//    public ReenterLock(ReentrantLock lock) {
+//        this.lock = lock;
+//    }
 
     @Override
     public void run() {
+        System.out.println(Thread.currentThread() + "-------------start");
         for (int j = 0; j < 100; j++) {
+            System.out.println(Thread.currentThread() + "-------------start" + j);
+//            lock.lock();多锁一次， 锁等待
             lock.lock();
             try {
                 i++;
             } finally {
                 lock.unlock();
+//                lock.unlock(); 少释放一次  Exception in thread "Thread-0" java.lang.IllegalMonitorStateException
             }
         }
 //        System.out.println(Thread.currentThread().getId() + ":" + i);
@@ -29,24 +33,23 @@ public class ReenterLock implements Runnable {
     public static void main(String[] args) throws InterruptedException {
 
 
-
-//        ReenterLock lock = new ReenterLock();
-//        Thread thread1 = new Thread(lock);
-//        Thread thread2 = new Thread(lock);
-//        thread1.start();
-//        thread2.start();
-//        thread1.join();
-//        thread2.join();
-//        System.out.println(i);
-        ReentrantLock reentrantLock=new ReentrantLock();
-        ReenterLock lock1 = new ReenterLock(reentrantLock);
-        ReenterLock lock2 = new ReenterLock(reentrantLock);
-        Thread thread1 = new Thread(lock1);
-        Thread thread2 = new Thread(lock2);
+        ReenterLock lock = new ReenterLock();
+        Thread thread1 = new Thread(lock);
+        Thread thread2 = new Thread(lock);
         thread1.start();
         thread2.start();
         thread1.join();
         thread2.join();
         System.out.println(i);
+//        ReentrantLock reentrantLock = new ReentrantLock();
+//        ReenterLock lock1 = new ReenterLock(reentrantLock);
+//        ReenterLock lock2 = new ReenterLock(reentrantLock);
+//        Thread thread1 = new Thread(lock1);
+//        Thread thread2 = new Thread(lock2);
+//        thread1.start();
+//        thread2.start();
+//        thread1.join();
+//        thread2.join();
+//        System.out.println(i);
     }
 }
