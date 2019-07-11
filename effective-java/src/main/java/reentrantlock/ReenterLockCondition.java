70b4b8e33c6fb2498179f39fe2180aa63bc8ec4f
白芷 <2397701392@qq.com>
2019-07-12 00:44:11 +0800

    add reentrantLock 重入锁 Condition

---
+++ effective-java/src/main/java/reentrantlock/ReenterLockCondition.java
@@ -0,0 +1,37 @@
+package reentrantlock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReenterLockCondition implements Runnable {
+    private static ReentrantLock lock = new ReentrantLock();
+    private static Condition condition = lock.newCondition();
+
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+            System.out.println("locking...");
+            condition.await();
+            System.out.println("going on...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReenterLockCondition reenterLockCondition=new ReenterLockCondition();
+
+        Thread thread=new Thread(reenterLockCondition);
+        thread.start();
+
+        Thread.sleep(1000);
+
+        lock.lock();
+        condition.signal();
+        lock.unlock();
+
+    }
+}
