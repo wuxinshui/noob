e66f47af22a24736861ce6fd64f4f57937c9f5ea
任富吉 <fuji.ren@bkjk.com>
2019-07-11 10:28:39 +0800

    每日一题

--- effective-java/src/main/java/string/Trivial.java
+++ effective-java/src/main/java/string/Trivial.java
@@ -9,5 +9,6 @@ public class Trivial {
     public static void main(String args[]) {
         System.out.print("H" + "a");
         System.out.print('H' + 'a');
+        System.out.print('H' + 'b');
     }
 }
