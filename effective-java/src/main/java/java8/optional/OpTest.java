53beae64512cb3e56d5279ec830a4184fc0d3f0f
任富吉 <fuji.ren@bkjk.com>
2019-07-25 17:16:38 +0800

    Optional

--- effective-java/src/main/java/java8/optional/OpTest.java
+++ effective-java/src/main/java/java8/optional/OpTest.java
@@ -13,14 +13,16 @@ public class OpTest {
     public static void main(String[] args) {
         //List<Integer> list = List.of(1, 2, 3, 4, 5);
         List<Integer> list = null;
-        System.out.println(Optional.ofNullable(list).orElse(List.of(0)));
-        System.out.println(Optional.ofNullable(list).orElseGet(() -> List.of(0)));
-        System.out.println(Optional.ofNullable(list).orElseThrow(RuntimeException::new));
+        //System.out.println(Optional.ofNullable(list).orElse(List.of(0)));
+        //System.out.println(Optional.ofNullable(list).orElseGet(() -> List.of(0)));
+        //System.out.println(Optional.ofNullable(list).orElseThrow(RuntimeException::new));
+        //System.out.println(Optional.ofNullable(list).ifPresent(o->o.parallelStream().findAny().ifPresent(u->u.toString())));
+
+        //System.out.println(Optional.of(list).orElse(List.of(0)));
+        //System.out.println(Optional.of(list).orElseGet(() -> List.of(0)));
+        //System.out.println(Optional.of(list).orElseThrow(RuntimeException::new));
 
 
-        System.out.println(Optional.of(list).orElse(List.of(0)));
-        System.out.println(Optional.of(list).orElseGet(() -> List.of(0)));
-        System.out.println(Optional.of(list).orElseThrow(RuntimeException::new));
     }
 
 }
