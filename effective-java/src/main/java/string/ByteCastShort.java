a4bbd4ff4d19fd0680cbef0f2bf59f58204f7c2b
任富吉 <fuji.ren@bkjk.com>
2019-08-22 11:16:00 +0800

    Byte cast short

---
+++ effective-java/src/main/java/string/ByteCastShort.java
@@ -0,0 +1,16 @@
+package string;
+
+/**
+ * @Author: yoyo
+ * @Description:
+ * @Date: Created in 2019/8/22 11:14
+ */
+public class ByteCastShort {
+    public static void main(String[] args) {
+        short a=128;
+        byte b= (byte) a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
