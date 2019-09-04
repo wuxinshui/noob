ed9436bbd70798e4f3d43cadcb78e2c658f839f1
任富吉 <fuji.ren@bkjk.com>
2019-09-04 10:47:35 +0800

    add link

--- effective-java/src/main/java/scriptengine/JavaJavaScript.java
+++ effective-java/src/main/java/scriptengine/JavaJavaScript.java
@@ -6,7 +6,8 @@ import javax.script.ScriptException;
 
 /**
  * @Author: yoyo
- * @Description:
+ * @Description: Run Javascript from Java <link>https://javapapers.com/core-java/run-javascript-from-java/</>
+ * Java 8 Features <link>https://javapapers.com/java/java-8-features/</link>
  * @Date: Created in 2019/9/4 10:38
  */
 public class JavaJavaScript {
@@ -16,5 +17,6 @@ public class JavaJavaScript {
         engine.eval("var x = 10;");
         engine.eval("var y = 20;");
         engine.eval("var z = x + y;");
-        engine.eval("print (z);");    }
+        engine.eval("print (z);");
+    }
 }
