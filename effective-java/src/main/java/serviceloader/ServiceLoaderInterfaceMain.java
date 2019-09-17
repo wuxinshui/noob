cd788806265ceb34954df6199ee983ed3aa9ee76
任富吉 <fuji.ren@bkjk.com>
2019-09-17 11:20:05 +0800

    add override

--- effective-java/src/main/java/serviceloader/ServiceLoaderInterfaceMain.java
+++ effective-java/src/main/java/serviceloader/ServiceLoaderInterfaceMain.java
@@ -3,17 +3,22 @@ package serviceloader;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
+/**
+ * 需要有：META-INF/services/serviceloader.ServiceLoaderInterface
+ *
+ * @author DELL
+ */
 public class ServiceLoaderInterfaceMain {
     public static void main(String[] args) {
-        ServiceLoader<ServiceLoaderInterface> loaderInterfaces=ServiceLoader.load(ServiceLoaderInterface.class);
+        ServiceLoader<ServiceLoaderInterface> loaderInterfaces = ServiceLoader.load(ServiceLoaderInterface.class);
         System.out.println("foreach print load impl...");
-        loaderInterfaces.stream().forEach(o->{
+        loaderInterfaces.stream().forEach(o -> {
             System.out.println(o.toString());
         });
         System.out.println("iterator print load impl...");
-        Iterator iterator= loaderInterfaces.iterator();
-        while (iterator.hasNext()){
-            ServiceLoaderInterface loaderInterface= (ServiceLoaderInterface) iterator.next();
+        Iterator iterator = loaderInterfaces.iterator();
+        while (iterator.hasNext()) {
+            ServiceLoaderInterface loaderInterface = (ServiceLoaderInterface) iterator.next();
             System.out.println(loaderInterface.toString());
         }
     }
