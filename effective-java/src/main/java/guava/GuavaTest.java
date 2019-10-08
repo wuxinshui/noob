26c23688f361409a1ca9c8957c19b873803d4b06
任富吉 <fuji.ren@bkjk.com>
2019-10-08 17:50:29 +0800

    Guava Stopwatch

--- effective-java/src/main/java/guava/GuavaTest.java
+++ effective-java/src/main/java/guava/GuavaTest.java
@@ -8,8 +8,9 @@ import com.google.common.base.Stopwatch;
  * @Date: Created in 2019/10/8 17:48
  */
 public class GuavaTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Stopwatch stopwatch = Stopwatch.createStarted();
+        Thread.sleep(10);
         stopwatch.stop();
         System.out.println(stopwatch.elapsed().getNano());
     }
