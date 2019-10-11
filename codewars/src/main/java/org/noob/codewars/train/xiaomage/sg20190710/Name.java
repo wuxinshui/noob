9483c4c338174a74557acfd07ba9f72f2767c664
任富吉 <fuji.ren@bkjk.com>
2019-10-11 18:19:54 +0800

    String 中的 equals 方法是被重写过的，因为 object 的 equals 方法是比较的对象的内存地址，而 String 的 equals 方法比较的是对象的值。

--- codewars/src/main/java/org/noob/codewars/train/xiaomage/sg20190710/Name.java
+++ codewars/src/main/java/org/noob/codewars/train/xiaomage/sg20190710/Name.java
@@ -1,8 +1,52 @@
 package org.noob.codewars.train.xiaomage.sg20190710;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 2019.07.10 期
  * https://segmentfault.com/q/1010000019716098
+ *
+ * 以上程序输出内容是？
+ *
+ * (a) true
+ * (b) false
+ * (c) 程序编译错误
+ * (d) 以上都不是
  */
 public class Name {
+    private String first, last;
+
+    public Name(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public static void main(String[] args) {
+        Set s = new HashSet();
+        s.add(new Name("Mickey", "Mouse"));
+        System.out.println(s.contains(new Name("Mickey", "Mouse")));
+    }
+    //没有重写equals()方法，走的是Object的equals()，和 == 一样。比较的是地址值
+    public boolean equals(Name o) {
+        //if (this == o) return true;
+        //if (o == null || getClass() != o.getClass()) return false;
+        //Name name = (Name) o;
+        return first.equals(o.first) && last.equals(o.last);
+
+    }
+
+
+    //@Override
+    //public boolean equals(Object o) {
+    //    //if (this == o) return true;
+    //    //if (o == null || getClass() != o.getClass()) return false;
+    //    Name name = (Name) o;
+    //    return first.equals(name.first) && last.equals(name.last);
+    //}
+
+    @Override
+    public int hashCode() {
+        return 31 * first.hashCode() + last.hashCode();
+    }
 }
