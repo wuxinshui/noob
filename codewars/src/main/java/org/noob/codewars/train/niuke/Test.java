fc5d5fff29b3294b7be4e8c58825cbca580326fc
任富吉 <fuji.ren@bkjk.com>
2019-10-14 10:18:17 +0800

    java.io.PrintStream.println(char[])

---
+++ codewars/src/main/java/org/noob/codewars/train/niuke/Test.java
@@ -0,0 +1,16 @@
+package org.noob.codewars.train.niuke;
+
+/**
+ * @Author: yoyo
+ * @Description:
+ * @Date: Created in 2019/10/12 16:31
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] st=new int[]{1,2,3};
+        System.out.println(st);
+        char[] st1=new char[]{'1','2','3'};
+        //java.io.PrintStream.println(char[])
+        System.out.println(st1);
+    }
+}
