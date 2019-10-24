c95d42f5f97f5bf2f3f5db9827c55dd3b00b5e83
rfg2012@163.com <secuity2012>
2019-10-24 22:47:39 +0800

    List Sort

---
+++ effective-java/src/main/java/java8/list/ListSort.java
@@ -0,0 +1,16 @@
+package java8.list;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSort {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+        Collections.sort(names, String::compareTo);
+        System.out.println(names);
+        Collections.sort(names,(String a,String b)->a.compareTo(b));
+        System.out.println(names);
+
+    }
+}
