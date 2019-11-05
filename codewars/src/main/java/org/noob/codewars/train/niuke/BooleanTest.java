34d99c136a992dd7ae310379ca3fd807609255c2
任富吉 <fuji.ren@bkjk.com>
2019-11-05 11:03:43 +0800

    Boolean = test

---
+++ codewars/src/main/java/org/noob/codewars/train/niuke/BooleanTest.java
@@ -0,0 +1,17 @@
+package org.noob.codewars.train.niuke;
+
+/**
+ * @Author: yoyo
+ * @Description:
+ * @Date: Created in 2019/11/5 11:02
+ */
+public class BooleanTest {
+    public static void main(String[] args) {
+        Boolean falg = false;
+        if (falg = true) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
