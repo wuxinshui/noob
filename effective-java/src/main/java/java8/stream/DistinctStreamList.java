0327d50457de0a3ee0c929774b40a129d1a6d950
任富吉 <fuji.ren@bkjk.com>
2019-12-10 16:40:57 +0800

    List<List> -> List

---
+++ effective-java/src/main/java/java8/stream/DistinctStreamList.java
@@ -0,0 +1,18 @@
+package java8.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: yoyo
+ * @Description:
+ * @Date: Created in 2019/12/10 16:26
+ */
+public class DistinctStreamList {
+    public static void main(String[] args) {
+        List<String> list=List.of("1","2","2","2","A","A","N");
+        System.out.println(list);;
+        list= list.parallelStream().distinct().collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
---
+++ effective-java/src/main/java/java8/stream/StreamUtils.java
@@ -0,0 +1,18 @@
+package java8.stream;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @Author: yoyo
+ * @Description:
+ * @Date: Created in 2019/12/10 16:28
+ */
+public class StreamUtils {
+    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+        Map<Object, Boolean> map = new ConcurrentHashMap<>();
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+}
