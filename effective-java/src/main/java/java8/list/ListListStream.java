05004a60884da779b172a03849c3110990ab384d
任富吉 <fuji.ren@bkjk.com>
2019-12-10 11:41:32 +0800

    List<List> -> List

--- effective-java/src/main/java/java8/list/ListListStream.java
+++ effective-java/src/main/java/java8/list/ListListStream.java
@@ -5,19 +5,19 @@ import java.util.stream.Collectors;
 
 /**
  * @Author: yoyo
- * @Description:
+ * @Description: List<List> convert to List<>
  * @Date: Created in 2019/12/10 11:37
  */
 public class ListListStream {
     public static void main(String[] args) {
-        List<Integer> list1=List.of(1,2,3,4,5);
-        List<Integer> list2=List.of(1,2,3,4,5);
-        List<Integer> list3=List.of(1,2,3,4,5);
-        List<List<Integer>> listList=List.of(list1,list2,list3);
+        List<Integer> list1 = List.of(1, 2, 3, 4, 5);
+        List<Integer> list2 = List.of(1, 2, 3, 4, 5);
+        List<Integer> list3 = List.of(1, 2, 3, 4, 5);
+        List<List<Integer>> listList = List.of(list1, list2, list3);
         System.out.println(listList);
 
-    //    List合并
-        List<Integer> oneList=listList.parallelStream().flatMap(List::stream).collect(Collectors.toList());
+        //    List合并
+        List<Integer> oneList = listList.parallelStream().flatMap(List::stream).collect(Collectors.toList());
 
         System.out.println(oneList);
     }
