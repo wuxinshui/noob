04614c49b321d120f9ec1efd57ad8fc733349738
任富吉 <fuji.ren@bkjk.com>
2020-01-16 18:19:08 +0800

    java8 LocalDateTime

--- effective-java/src/main/java/java8/localdatetime/LocalDateTimeTest.java
+++ effective-java/src/main/java/java8/localdatetime/LocalDateTimeTest.java
@@ -10,17 +10,21 @@ import java.time.ZoneOffset;
  */
 public class LocalDateTimeTest {
     public static void main(String[] args) {
-        //    获取秒
+        //打印当前时间
+        System.out.println(LocalDateTime.now());
+        //当前时间-秒
         long second = LocalDateTime.now().getSecond();
         System.out.println(second);
-        //long second1 = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
-        long second1 = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
-        System.out.println("second1 "+second1);
+        //the number of seconds from the epoch of 1970-01-01T00:00:00Z
+        long second1 = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
+        System.out.println("second1 " + second1);
+        //Gets the second-of-minute field.
         long sd = LocalDateTime.now().atOffset(ZoneOffset.of("+8")).getSecond();
-        System.out.println("getSecond "+sd);
+        System.out.println("getSecond " + sd);
+        //the number of seconds from the epoch of 1970-01-01T00:00:00Z
         long sd1 = LocalDateTime.now().atOffset(ZoneOffset.of("+8")).toEpochSecond();
-        System.out.println("toEpochSecond "+sd1);
-        //    毫秒
+        System.out.println("toEpochSecond " + sd1);
+        //毫秒
         long ms = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
         System.out.println(ms);
     }
