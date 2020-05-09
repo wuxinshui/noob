98c6e39fdf50471ded4c73387d0ae8087b54602c
yoyo <yoyo@yoyo.com>
2020-05-09 15:52:11 +0800

    String 2 Long

--- effective-java/src/main/java/string/String2Long.java
+++ effective-java/src/main/java/string/String2Long.java
@@ -2,7 +2,7 @@ package string;
 
 public class String2Long {
     public static void main(String[] args) {
-        String code = "1000001000161060";
+        String code = "1000001000000000";
         System.out.println(code.replaceFirst("1", "0"));
         System.out.println(code.replaceFirst("0", ""));
         String number = code.replaceFirst("1", "0");
