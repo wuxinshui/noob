d2b3d68c77a18e0e4fecb8096adf80a4add275ef
yoyo <yoyo@yoyo.com>
2020-05-09 15:56:43 +0800

    String 2 Long

--- effective-java/src/main/java/string/String2Long.java
+++ effective-java/src/main/java/string/String2Long.java
@@ -7,5 +7,12 @@ public class String2Long {
         System.out.println(code.replaceFirst("0", ""));
         String number = code.replaceFirst("1", "0");
         System.out.println(Long.parseLong(number));
+
+        String max_code = "100000000000000000";
+        String min_code = "123456";
+
+        String md = max_code.substring(0, max_code.length() - min_code.length()).concat(min_code);
+        System.out.println(max_code);
+        System.out.println(md);
     }
 }
