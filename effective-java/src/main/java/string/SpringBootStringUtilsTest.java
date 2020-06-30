3dd7ffba5c9711848a0076a4ae108a7ab2610340
yoyo <yoyo@yoyo.com>
2020-06-30 14:12:46 +0800

    StringUtils.delimitedListToStringArray

--- effective-java/src/main/java/string/SpringBootStringUtilsTest.java
+++ effective-java/src/main/java/string/SpringBootStringUtilsTest.java
@@ -16,23 +16,25 @@ public class SpringBootStringUtilsTest {
         // String str = "";
         // String str = "330200";
         String[] array = StringUtils.split(str, ",");
-        System.out.println(Arrays.toString(array));
+        System.out.println(array.length+" : "+Arrays.toString(array));
     }
 
     public static void stringSplitTest() {
-        String str = "330200,330300,331000,330700,330100,330600,330500,330400,320500,320200,320600,320400,320100,320300,320800,321100,320900";
-        // String str = "";
+        // String str = "330200,330300,331000,330700,330100,330600,330500,330400,320500,320200,320600,320400,320100,320300,320800,321100,320900";
+        String str = "";
+        // String str = null;
         // String str = "330200";
         String[] array = str.split(",");
-        System.out.println(Arrays.toString(array));
+        System.out.println(array.length+" : "+Arrays.toString(array));
     }
 
     public static void delimitedListToStringArrayTest() {
-        String str = "330200,330300,331000,330700,330100,330600,330500,330400,320500,320200,320600,320400,320100,320300,320800,321100,320900";
+        // String str = "330200,330300,331000,330700,330100,330600,330500,330400,320500,320200,320600,320400,320100,320300,320800,321100,320900";
         // String str = "";
+        String str = null;
         // String str = "330200";
 
         String[] array = StringUtils.delimitedListToStringArray(str, ",");
-        System.out.println(Arrays.toString(array));
+        System.out.println(array.length+" : "+Arrays.toString(array));
     }
 }
